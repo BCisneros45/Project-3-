4913ef8d70b48fad0ebd6314cdab447dc917b368
Bryan <0964560@my.scccd.edu>
2025-12-05 06:56:22 +0000

    commitment 4

--- BCProj3_1.m
+++ BCProj3_1.m
@@ -11,7 +11,8 @@ y2 = input('Enter y2: ');
 
 x3 = input('Enter x3: ');
 y3 = input('Enter y3: ');
-
+% Solve a system of equations to determine the coefficients a, b, and c for
+%  the equation for the parabola
 A = [x1^2  x1  1;
      x2^2  x2  1;
      x3^2  x3  1];
@@ -22,3 +23,11 @@ coeff = A/b;
 a = coeff(1);
 bcoef = coeff(2);
 c = coeff(3);
+% Display the equation for that parabola on the screen
+fprintf('\nThe parabola passing through the three points is: \n)');
+fprintf(' y = %.3f x^2 %+ .3f x %= .3f\n\n',a, bcoef, c);
+
+x = -10:0.1:10;
+y = a*x.^2+ bcoef*x + c;
+
+%
