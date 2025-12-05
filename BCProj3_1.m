bbe5d60fa2f5fa260207fdb87d2c268a02e0bdb7
BCisneros45 <cisnerosbryan137@gmail.com>
2025-12-04 20:43:00 -0800

    Add files via upload

---
+++ BCProj3_1.m
@@ -0,0 +1,13 @@
+% Bryan Cisneros
+% Project 3_1
+
+%Ask the user for three sets of (x,y) 
+% coordinates that a parabola passes through.
+x1 = input('Enter x1: ');
+y1 = input('Enter y1: ');
+
+x2 = input('Enter x2: ');
+y2 = input('Enter y2: ');
+
+x3 = input('Enter x3: ');
+y3 = input('Enter y3: ');
